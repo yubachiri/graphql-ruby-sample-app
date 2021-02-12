9b7bc0c1f0befe33499ba981706aa10c3af626a0
yubachiri <k.i.moso9532@gmail.com>
2021-02-12 16:31:08 +0900

    不要なルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
 
-  root to: 'view#show'
-
   if Rails.env.development?
-    post '/schema', to: 'graphql_schema#execute'
     get '/graphiql', to: 'graphiql#show'
-    mount LetterOpenerWeb::Engine, at: '/letter_opener'
   end
-
-  get '*path' => 'view#show', constraints: lambda { |req|
-    req.path.exclude? 'rails/active_storage'
-  }
 end
